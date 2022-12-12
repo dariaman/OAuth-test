a56b24f702671e4ccb96b4ad1adda10c854f1036
dariaman <dariaman.siagian@gmail.com>
2022-12-12 16:48:22 +0700

    update login google

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CredentialResponse } from 'google-one-tap';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'OAuth-test';
+
+  LoginModel: any = {}
+
+  handleCredentialGoogleResponse(response: CredentialResponse) {
+    console.log(response);
+  }
+
+  
 }
